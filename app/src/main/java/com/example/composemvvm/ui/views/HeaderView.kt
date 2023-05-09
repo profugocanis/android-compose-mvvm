0a3c1acf1d6010a3d1a0c3696d7a2e51d068b1fd
olehkyrhan <oleh.kyrhan@gmail.com>
2023-05-09 16:17:54 +0300

    Commit

--- app/src/main/java/com/example/composemvvm/ui/views/HeaderView.kt
+++ app/src/main/java/com/example/composemvvm/ui/views/HeaderView.kt
@@ -2,11 +2,19 @@ package com.example.composemvvm.ui.views
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import com.example.composemvvm.extentions.isRtl
 import com.example.composemvvm.extentions.onBounceClick
 
 @Composable
@@ -27,15 +35,23 @@ fun HeaderView(title: String, onBack: (() -> Unit)? = null) {
         })
 
         if (onBack != null) {
-            Text(text = "<Back", modifier = Modifier
-                .constrainAs(backView) {
-                    top.linkTo(parent.top)
-                    bottom.linkTo(parent.bottom)
-                    start.linkTo(parent.start, margin = 16.dp)
-                }
-                .onBounceClick {
-                    onBack()
-                })
+            val context = LocalContext.current
+            Icon(
+                Icons.Outlined.ArrowBack,
+                contentDescription = null,
+                tint = Color.Black,
+                modifier = Modifier
+                    .padding(end = 2.dp)
+                    .constrainAs(backView) {
+                        top.linkTo(parent.top)
+                        bottom.linkTo(parent.bottom)
+                        start.linkTo(parent.start, margin = 16.dp)
+                    }
+                    .scale(scaleX = if (context.isRtl) -1f else 1f, scaleY = 1f)
+                    .onBounceClick {
+                        onBack()
+                    }
+            )
         }
     }
 }
